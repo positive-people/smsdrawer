58888916bda91cd67e58ca8af82788d9fdb01705
jihoon <hoony@hoony.org>
2018-09-12 13:56:46 +0900

    Initial commit

---
+++ app/src/main/java/org/hoony/test/smsdrawer/MainActivity.java
@@ -0,0 +1,20 @@
+package org.hoony.test.smsdrawer;
+
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setBackground(new ShapeDrawable(new OvalShape()));
+        imageView.setClipToOutline(true);
+
+    }
+}
